138efeeddcf81394b6f485a0b8f726b573bc4964
Bryden Arndt <nedyrb@gmail.com>
2015-01-16 11:31:14 -0500

    cleaned the endorsements public visitor view

--- mod/b_extended_profile/views/default/b_extended_profile/endorsements.php
+++ mod/b_extended_profile/views/default/b_extended_profile/endorsements.php
@@ -13,7 +13,7 @@ if(!(is_array($skill_guids))) {
     $skill_guids = array($skill_guids);
 }
 
-if (is_array($skill_guids)) {
+if (is_array($skill_guids) && elgg_is_logged_in()) {
     foreach($skill_guids as $skill_guid) {
         $skill = get_entity($skill_guid);
         $skill_class =  str_replace(' ', '-', strtolower($skill->title));
@@ -25,6 +25,7 @@ if (is_array($skill_guids)) {
 
             if (elgg_get_page_owner_guid() != elgg_get_logged_in_user_guid()) {
                 if(in_array(elgg_get_logged_in_user_guid(), $endorsements) == false || empty($endorsements)) {
+                    // user has not yet endorsed this skill for this user.. present the option to endorse
                     error_log('SKILL: ' . $skill->title);
                     error_log('Logged in user: ' . elgg_get_logged_in_user_guid());
                     error_log('Endorsements: ' . $endorsements);
@@ -33,11 +34,12 @@ if (is_array($skill_guids)) {
                     echo '<span class="gcconnex-endorsement-add add-endorsement-' . $skill_class . '" onclick="addEndorsement(this)" data-guid="' . $skill->guid . '" data-skill="' . $skill->title . '">+</span>';
                 }
                 else {
+                    // user has endorsed this skill for this user.. present the option to retract endorsement
                     echo '<span class="gcconnex-endorsement-retract retract-endorsement-' . $skill_class . '" onclick="retractEndorsement(this)" data-guid="' . $skill->guid . '" data-skill="' . $skill->title . '">-</span>';
                     error_log('SKILL: ' . $skill->title);
                     error_log('Logged in user: ' . elgg_get_logged_in_user_guid());
-                    error_log('Endorsements: ' . $skill->endorsements);
-                    error_log('Search result: ' . in_array(elgg_get_logged_in_user_guid(), $skill->endorsements));
+                    error_log('Endorsements: ' . $endorsements);
+                    error_log('Search result: ' . in_array(elgg_get_logged_in_user_guid(), $endorsements));
                 }
             }
         // @todo: add the endorsing user's profile image to the list of endorsers for this skill
@@ -47,11 +49,16 @@ if (is_array($skill_guids)) {
     }
 }
 else {
+
+
+    /*
+
     $skill = get_entity($skill_guids);
 //    $skillClass = str_replace(" ", "-", strtolower($skill->title));
     echo '<div class="gcconnex-skill-entry" data-guid="' . $skill_guids . '">';
         echo '<div class="gcconnex-endorsements-count">0</div><div class="gcconnex-endorsements-skill" data-type="skill">' . $skill->title . '</div>';
     echo '</div>';
+    */
 }
 
 echo '</div></div><div class="endorsements-message"></div>';
