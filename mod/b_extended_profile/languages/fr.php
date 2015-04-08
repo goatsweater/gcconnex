9f19f547d2213a9cc0445465ce40dd5b4768452c
Bryden Arndt <nedyrb@gmail.com>
2015-04-08 14:59:05 -0400

    added wire post quotes.png and french language file

---
+++ mod/b_extended_profile/languages/fr.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: barndt
+ * Date: 30/01/15
+ * Time: 2:51 PM
+ */
+
+$french = array(
+
+    // MAIN PROFILE PAGE
+    'gcconnex_profile:edit_profile' => 'Modifier',
+    'gcconnex_profile:user_content' => 'Contenu personnel',
+
+    'gcconnex_profile:about_me' => 'À mon sujet',
+    'gcconnex_profile:education' => 'Études',
+    'gcconnex_profile:experience' => 'Expérience de travail',
+    'gcconnex_profile:gc_skills' => 'Compétences',
+    'gcconnex_profile:edit' => 'Modifier',
+    'gcconnex_profile:cancel' => 'Annuler',
+    'gcconnex_profile:save' => 'Enregistrer',
+    'gcconnex_profile:present' => 'à présent', // for work and education time ranges.. from XX date to Present
+    'gcconnex_profile:about_me:empty' => 'Saisissez une description de vous-même en cliquant sur "Modifier" dans le coin supérieur droit de cette zone de saisie.',// prompt to add first skill
+
+
+    // BASIC PROFILE FORM
+    'gcconnex_profile:basic:header' => 'Modifier le profil de base',
+    'gcconnex_profile:basic:name' => 'Nom : ',
+    'gcconnex_profile:basic:title' => 'Titre : ',
+    'gcconnex_profile:basic:department' => 'Ministère : ',
+    'gcconnex_profile:basic:phone' => 'Téléphone : ',
+    'gcconnex_profile:basic:mobile' => 'Cellulaire : ',
+    'gcconnex_profile:basic:email' => 'Courriel : ',
+    'gcconnex_profile:basic:website' => 'Site Web : ',
+    'gcconnex_profile:basic:micro_confirmation' => 'Les micro-affectations sont des affectations de courte durée publiées sur GCconnex et offertes à tous les fonctionnaires, sans égard à leur ministère. Elles sont de courte durée et de portée restreinte.<p>Avant de présenter votre candidature pour les postes offerts dans le cadre de micro-affectations, vous devez d\'abord obtenir l\'autorisation de votre gestionnaire, puis cliquer sur la case à cocher ci-dessous.',
+    'gcconnex_profile:basic:micro_checkbox' => 'Je souhaite présenter ma candidature pour les postes offerts dans le cadre de micro-affectations.',
+    'gcconnex_profile:basic:save' => 'Enregistrer',
+
+    // MONTHS
+    'gcconnex_profile:month:january' => 'Janvier',
+    'gcconnex_profile:month:february' => 'Février',
+    'gcconnex_profile:month:march' => 'Mars',
+    'gcconnex_profile:month:april' => 'Avril',
+    'gcconnex_profile:month:may' => 'Mai',
+    'gcconnex_profile:month:june' => 'Juin',
+    'gcconnex_profile:month:july' => 'Juillet',
+    'gcconnex_profile:month:august' => 'Août',
+    'gcconnex_profile:month:september' => 'Septembre',
+    'gcconnex_profile:month:october' => 'Octobre',
+    'gcconnex_profile:month:november' => 'Novembre',
+    'gcconnex_profile:month:december' => 'Décembre',
+
+    // EDUCATION
+    'gcconnex_profile:education:school' => 'SNom de l\'établissement d\'enseignement : ',
+    'gcconnex_profile:education:start_month' => 'Mois de début : ',
+    'gcconnex_profile:education:year' => 'Année : ',
+    'gcconnex_profile:education:end_month' => 'Mois de fin : ',
+    'gcconnex_profile:education:ongoing' => 'J\'étudie encore dans cet établissement d\'enseignement',
+    'gcconnex_profile:education:program' => 'Programme : ',
+    'gcconnex_profile:education:field' => 'Domaine d\'études : ',
+    'gcconnex_profile:education:delete' => 'Supprimer cette entrée',
+    'gcconnex_profile:education:add' => '+ ajouter d\'autres études',
+    'gcconnex_profile:education:access' => 'Permettre l\'affichage des renseignements sur les études par : ',
+    'gcconnex_profile:education:present' => 'à présent',
+    'gcconnex_profile:education:empty' => 'Saisissez les renseignements sur vos études en cliquant sur "Modifier" dans le coin supérieur droit de cette zone de saisie.',
+
+    // WORK EXPERIENCE
+    'gcconnex_profile:experience:organization' => 'Nom de l’organisation : ',
+    'gcconnex_profile:experience:title' => 'Titre : ',
+    'gcconnex_profile:experience:start_month' => 'Mois de début : ',
+    'gcconnex_profile:experience:year' => 'Année : ',
+    'gcconnex_profile:experience:end_month' => 'Mois de fin : ',
+    'gcconnex_profile:experience:ongoing' => 'J\'occupe encore ce poste',
+    'gcconnex_profile:experience:responsibilities' => 'Responsabilités',
+    'gcconnex_profile:experience:delete' => 'Supprimer cette entrée',
+    'gcconnex_profile:experience:add' => '+ ajouter d\'autres renseignements sur l\'expérience de travail',
+    'gcconnex_profile:experience:access' => 'Permettre l\'affichage des renseignements sur l\'expérience de travail par : ',
+    'gcconnex_profile:experience:present' => 'à présent',
+    'gcconnex_profile:experience:colleagues' => 'Collègues : ',
+    'gcconnex_profile:experience:empty' => 'Saisissez les renseignements sur votre expérience de travail en cliquant sur "Modifier" dans le coin supérieur droit de cette zone de saisie.',
+    'gcconnex_profile:experience:colleagues_empty' => 'Vous n\'avez inscrit aucun nom de collègue dans cette zone de saisie.',
+
+    // SKILLS
+    'gcconnex_profile:gc_skill:add' => '+ ajouter une nouvelle compétence',
+    'gcconnex_profile:gc_skill:delete' => 'Supprimer cette compétence',
+    'gcconnex_profile:gc_skill:save' => 'Voulez-vous vraiment enregistrer ces modifications? Toutes les recommandations relatives aux compétences que vous avez supprimées seront effacées de façon permanente.',
+    'gcconnex_profile:gc_skill:empty' => 'Saisissez les renseignements sur vos compétences en cliquant sur "Modifier" dans le coin supérieur droit de cette zone de saisie.',
+    // leftover skills
+
+    // LANGUAGES
+    'gcconnex_profile:langs' => 'Langues',
+    'gcconnex_profile:languages:access' => 'Permettre l\'affichage des renseignements sur les langues par : ',
+    'gcconnex_profile:languages:language' => 'Langue :',
+    'gcconnex_profile:languages:add' => '+ ajouter d\'autres langues',
+    'gcconnex_profile:languages:delete' => 'Supprimer cette entrée',
+    'gcconnex_profile:languages:empty' => 'Saisissez les renseignements sur les langues en cliquant sur "Modifier" dans le coin supérieur droit de cette zone de saisie.',
+    'gcconnex_profile:languages:writtencomp' => 'Compréhension écrite',
+    'gcconnex_profile:languages:writtenexp' => 'Expression écrite',
+    'gcconnex_profile:languages:oral' => 'Compétence orale',
+    'gcconnex_profile:languages:english' => 'Anglais',
+    'gcconnex_profile:languages:french' => 'Français',
+    'gcconnex_profile:languages:expiry' => 'Expiration',
+
+    // PUBLICATIONS
+    'gcconnex_profile:publications:empty' => 'This section will be added soon. Please check back later.'
+
+);
+
+add_translation("fr",$french);
